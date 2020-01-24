d9a5b2db63fdb810bdd6865d65f078060f77e80c
Doronlnd84 <doronlnd84@gmail.com>
2020-01-24 12:07:31 +0200

    Add

--- src/main/java/controllers/CatalogController.java
+++ src/main/java/controllers/CatalogController.java
@@ -9,7 +9,7 @@ import models.Template;
 @RestController
 public class CatalogController {
   
-	
+//‎1.‎	Calling to greetingCard/catalog will return all the templates of the greeting card system :	 
 	@GetMapping({"/greetingCard/catalog‎"})
 	public String[] greeting()  {
 		String[] templateTypes = {"Birthday","NewYear"} ;
